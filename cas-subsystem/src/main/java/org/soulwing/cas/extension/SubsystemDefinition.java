9eec78e28b5ab84d1f0fe8ad58e56c0966cca922
Carl Harris <ceharris@vt.edu>
2015-02-16 07:06:04 -0500

    folded step handlers into subsystem definition
    Just for consistency with the other definitions in the model.

--- cas-subsystem/src/main/java/org/soulwing/cas/extension/SubsystemDefinition.java
+++ cas-subsystem/src/main/java/org/soulwing/cas/extension/SubsystemDefinition.java
@@ -18,9 +18,21 @@
  */
 package org.soulwing.cas.extension;
 
+import java.util.List;
+
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
+import org.jboss.as.controller.AbstractRemoveStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceController;
+import org.soulwing.cas.deployment.SubsystemDeploymentProcessor;
 
 
 /**
@@ -50,6 +62,65 @@ public class SubsystemDefinition extends SimpleResourceDefinition {
   @Override
   public void registerAttributes(
       ManagementResourceRegistration resourceRegistration) {
+  }
+
+  static class SubsystemAdd extends AbstractBoottimeAddStepHandler {
+
+    static final SubsystemAdd INSTANCE = new SubsystemAdd();
+
+    private SubsystemAdd() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void populateModel(ModelNode operation, ModelNode model)
+        throws OperationFailedException {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void performBoottime(OperationContext context, ModelNode operation,
+        ModelNode model, ServiceVerificationHandler verificationHandler,
+        List<ServiceController<?>> newControllers)
+        throws OperationFailedException {
+
+      // Add deployment processors here
+      // Remove this if you don't need to hook into the deployers, or you can add
+      // as many as you like
+      // see SubDeploymentProcessor for explanation of the phases
+      context.addStep(new AbstractDeploymentChainStep() {
+        public void execute(DeploymentProcessorTarget processorTarget) {
+          processorTarget.addDeploymentProcessor(
+              Names.SUBSYSTEM_NAME,
+              SubsystemDeploymentProcessor.PHASE,
+              SubsystemDeploymentProcessor.PRIORITY,
+              new SubsystemDeploymentProcessor());
+
+        }
+      }, OperationContext.Stage.RUNTIME);
+
+    }
+  }
+  
+  static class SubsystemRemove extends AbstractRemoveStepHandler {
+
+    static final SubsystemRemove INSTANCE = new SubsystemRemove();
+
+    private SubsystemRemove() {
+    }
+
+    @Override
+    protected void performRuntime(OperationContext context, 
+        ModelNode operation, ModelNode model) throws OperationFailedException {
+        //Remove any services installed by the corresponding add handler here
+        //context.removeService(ServiceName.of("some", "name"));
+    }
+
   }
 
 }
