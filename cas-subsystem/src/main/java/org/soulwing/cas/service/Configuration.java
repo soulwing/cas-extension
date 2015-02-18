1579a23c08a7549600c0fc4d8bef3cdceb5945a9
Carl Harris <ceharris@vt.edu>
2015-02-18 06:14:13 -0500

    javadoc cleanup

--- cas-subsystem/src/main/java/org/soulwing/cas/service/Configuration.java
+++ cas-subsystem/src/main/java/org/soulwing/cas/service/Configuration.java
@@ -24,36 +24,99 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 
 /**
- * An (immutable) configuration for an {@link AuthenticationService}.
+ * An (immutable) configuration for an {@link Authenticator}.
  *
  * @author Carl Harris
  */
 public interface Configuration {
 
-  SSLContext getSslContext();
-  
-  HostnameVerifier getHostnameVerifier();
-  
+  /**
+   * Gets the protocol.
+   * @return protocol
+   */
   AuthenticationProtocol getProtocol();  
   
+  /**
+   * Gets the character encoding.
+   * @return character encoding
+   */
   String getEncoding();
 
+  /**
+   * Gets the CAS server URL.
+   * @return server URL
+   */
   String getServerUrl();
   
+  /**
+   * Gets the application service URL.
+   * @return service URL
+   */
   String getServiceUrl();
   
+  /**
+   * Gets the CAS proxy callback path.
+   * @return proxy callback path
+   */
   String getProxyCallbackUrl();
   
+  /**
+   * Gets a flag indicating whether any proxy should be accepted in a
+   * proxy ticket validation.
+   * @return flag state
+   */
   boolean isAcceptAnyProxy();
   
+  /**
+   * Gets a flag indicating whether an empty proxy chain should be accepted
+   * in a proxy ticket validation.
+   * @return flag state
+   */
   boolean isAllowEmptyProxyChain();
   
+  /**
+   * Gets the list of proxy chains that should be allowed in a proxy ticket
+   * validation.
+   * @return list of proxy chains
+   */
   List<String[]> getAllowedProxyChains();
   
+  /**
+   * Gets a flag indicating whether the user should be compelled to 
+   * re-authenticate.
+   * @return flag state
+   */
   boolean isRenew();
   
+  /**
+   * Gets the amount of time-of-day clock skew that is considered acceptable
+   * when evaluating the validity of SAML responses.
+   * @return skew tolerance in milliseconds
+   */
   long getClockSkewTolerance();
   
+  /**
+   * Gets a flag indicating whether, after a successful ticket validation, an
+   * additional redirect should be sent to the user's browser to remove 
+   * protocol-related parameters from the query string.
+   * @return flag state
+   */
   boolean isPostAuthRedirect();
   
+  /**
+   * Gets the SSL context to use in negotiations with the CAS server.
+   * @return SSL context or {@code null} to indicate that the default SSL
+   *    context is to be used
+   */
+  SSLContext getSslContext();
+  
+  /**
+   * Gets the hostname verifier to be used to verify the name of the CAS
+   * server (e.g. to workaround mismatches between server name and the name
+   * on the server's X.509 certificate).
+   * @return hostname verifier or {@code null} to indicate that the default
+   *    hostname verifier is to be used
+   */
+  HostnameVerifier getHostnameVerifier();
+
 }
